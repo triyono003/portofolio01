d7a933b4a7f1a71d0a16b8742ae26373cfe4ee4b
triyono003 <catatancoding07@gmail.com>
2023-07-17 16:40:48 +0700

    portofolio html css

---
+++ js/main.js
@@ -0,0 +1,8 @@
+let li1 = document.getElementsByTagName('li')[1];
+li1.addEventListener('click',function(){
+  location.href="/sertifikat/file.html";
+});
+
+function back(){
+  location.href="/../index.html";
+}
